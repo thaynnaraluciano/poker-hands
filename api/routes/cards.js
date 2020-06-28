685cc64092ea4456ce52f769bba37e08cea9f25b
thaynnaraluciano <thaynnara@LAPTOP-TK5SB851.localdomain>
2020-06-28 19:39:29 -0300

    Removing post method of cards

--- api/routes/cards.js
+++ api/routes/cards.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require ('mongoose');
 const Card = require('../models/cards');
 
 router.get('/', (req, res, next) => {
@@ -8,23 +7,6 @@ router.get('/', (req, res, next) => {
     .exec()
     .then(docs => {
         res.status(200).json(docs);
-    })
-});
-
-router.post('/', (req, res, next) => {
-    console.log(req);
-    const card = new Card({
-        _id: new mongoose.Types.ObjectId(),
-        symbol: req.body.symbol,
-        suit: req.body.suit
-    });
-    card.save()
-    .then(result => {
-        console.log(result);
-    })
-    .catch(err => console.log(err));
-    res.status(201).json({
-        createCard: card
     })
 });
 
